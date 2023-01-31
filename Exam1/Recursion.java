dc25160341b463da372565e803221826b0e57b63
Aguilar, Paul Andrei A <119924415+MomoTzuZaki@users.noreply.github.com>
2023-01-31 23:59:15 +0800

    Add files via upload

---
+++ Exam1/Recursion.java
@@ -0,0 +1,59 @@
+public class Recursion {
+
+    public static void main(String[] args) {
+
+        // Remove letters (30 pts)
+        // Instructions (Important!)
+        // Without using arrays or loops or regular expressions, apply recursion to remove specific letters in
+        // your full name.
+        // 1. If the last letter of your surname is a vowel, remove all vowels in
+        // your full name.
+        // 2. If the last letter of your surname is a consonant, remove all
+        // consonants in your full name.
+        // 3. Use only lowercase letters
+        // 4. Each letter should be removed one by one.
+        // Example:
+        // Full name is "elizer ponio jr".
+        // The output should be "lzr pn jr".
+        // Put your complete name in the "fullName" variable.
+        // Example:
+        // String fullName = "elizer ponio jr";
+
+        String fullName = "paul andrei aguilar";
+        removeLetter(fullName);
+    }
+
+    static void  removeLetter(String str) {
+        // Add code here
+            StringBuilder removeStr = new StringBuilder(str);
+            System.out.println(removeStr);
+            removeStr.deleteCharAt(0);
+            System.out.println(removeStr);
+
+            removeStr.deleteCharAt(2);
+            System.out.println(removeStr);
+
+            removeStr.deleteCharAt(4);
+            System.out.println(removeStr);
+
+            removeStr.deleteCharAt(4);
+            System.out.println(removeStr);
+
+            removeStr.deleteCharAt(4);
+            System.out.println(removeStr);
+
+            removeStr.deleteCharAt(8);
+            System.out.println(removeStr);
+
+            removeStr.deleteCharAt(10);
+            System.out.println(removeStr);
+
+            removeStr.deleteCharAt(9);
+            System.out.println(removeStr);
+
+            removeStr.deleteCharAt(10);
+            System.out.println(removeStr);
+
+        }
+    }
+
